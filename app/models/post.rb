a4a41cd66dde8b8c7332481127ce03eab5da9507
Nick Nobile <nickcnobile@gmail.com>
2021-02-02 00:18:50 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,16 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+
+  def categories_attributes=(categories_hashes)
+    categories_hashes.each do |i, category_attributes|
+      if category_attributes[:name].present?
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        if !self.categories.include?(category)
+        self.post_categories.build(:category => category)
+        end
+    end
+  end
+end
+
 end
